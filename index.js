5526b93d4ec6934ea0a2a4b6cebeed84f63ddf6d
Romain Al <romainal@gmail.com>
2024-11-08 12:32:01 +0100

    ok multi pico

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const net = require('net');
 const io = require('socket.io-client');
-const socket = io.connect("https://mypicoserv-unuojesj3q-od.a.run.app");
+// const socket = io.connect("http://192.168.10.2:1337");
+const socket = io.connect("https://pico-server-late-breeze-8245.fly.dev");
 socket.emit("join", "Pico", true);
-
-const host = "192.168.10.4";
 
 socket.on("mess", function (mess) {
     sendSock(mess);
@@ -12,18 +11,31 @@ socket.on("mess", function (mess) {
 
 function sendSock(mess){
     console.log(mess);
-    //console.log('answer received and sent to Pico');
+    let host;
+    switch (mess[0]=="N"?mess[2]:mess[0]){
+        case "A":
+            host = "192.168.10.6";
+            break;
+        case "B":
+            host = "192.168.10.7";
+            break;
+        case "C":
+            host = "192.168.10.8";
+            break;
+        case "D":
+            host = "192.168.10.9";
+            break;
+        default:
+            console.log(`MyError01: ${mess[0]}`);
+            break;
+    }
     let client = net.createConnection(3000, host, () => {
-        //console.log("Connected");
-        client.write(mess);
+        client.write((mess[0]=="N"?`NoVib${mess[mess.length-1]}`:`Vib${mess[mess.length-1]}`));
     });
     client.on("data", (data) => {
-        //console.log(`Received: ${data}`);
+        console.log(`Received: ${data}`);
     });
     client.on("error", (error) => {
         console.log(`Error: ${error.message}`);
-    });
-    client.on("close", () => {
-        //console.log("Connection closed");
     });
 }
